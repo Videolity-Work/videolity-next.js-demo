6186c79135739312855f9669dc728be6f0652b5c
damlaulku <damla@videolity.com>
2024-10-31 13:06:39 +0300

    link changed, cleaning

--- src/components/player/video-player.js
+++ src/components/player/video-player.js
@@ -1,42 +1,47 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from "react";
 
-const VIDEO_URL="https://staging.vdlty.co/video-player?id=30ca1a03-c339-4ce7-85c1-920155b69dc4&viewRatio=1920:950"
+const VIDEO_URL =
+  "https://staging.vdlty.co/video-player?id=4b0b9a9a-a7ef-4d91-b2c9-2bd2e4a2d1cb&viewRatio=1920:950";
 
 export default function VideoPlayer() {
-    const ref = useRef(null);
-    const [width, setWidth] = useState(0);
+  const ref = useRef(null);
+  const [width, setWidth] = useState(0);
 
-    // You can read the aspect ratio of the video from the viewRatio value in the video URL
-    // or you can statically write it here
-    const aspectRatio = 1920 / 950;
+  // You can read the aspect ratio of the video from the viewRatio value in the video URL
+  // or you can statically write it here
+  const aspectRatio = 1920 / 950;
 
-    // Update the video size when the screen size changes
-    const handleResize = () => {
-        const iframe = document.querySelector('iframe');
-        const width = iframe.offsetWidth;
-        iframe.style.height = `${width / aspectRatio}px`;
+  // Update the video size when the screen size changes
+  const handleResize = () => {
+    const iframe = document.querySelector("iframe");
+    const width = iframe.offsetWidth;
+    iframe.style.height = `${width / aspectRatio}px`;
+  };
+
+  // Update the width on the initial render and when the size changes
+  useEffect(() => {
+    const resize = () => {
+      setWidth(ref.current ? ref.current.offsetWidth : 0);
     };
+    resize();
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
 
-    // Update the width on the initial render and when the size changes
-    useEffect(() => {
-        const resize = () => {
-          setWidth(ref.current ? ref.current.offsetWidth : 0);
-        };
-        resize();
-        window.addEventListener('resize', resize);
-        return () => window.removeEventListener('resize', resize);
-      }, []);
-      
-      // Update the video size when the width changes
-      useEffect(() => {
-        handleResize();
-      }, [width]);
+  // Update the video size when the width changes
+  useEffect(() => {
+    handleResize();
+  }, [width]);
 
-    return (
-        <div style={{width: "100%"}}>
-            <iframe ref={ref} style={{width: "100%", height: "100%"}} src={VIDEO_URL}
-                    frameBorder="0"
-                    scrolling="no"></iframe>
-        </div>
-    );
+  return (
+    <div style={{ width: "100%" }}>
+      <iframe
+        ref={ref}
+        style={{ width: "100%", height: "100%" }}
+        src={VIDEO_URL}
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
 }
